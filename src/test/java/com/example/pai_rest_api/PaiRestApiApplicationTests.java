7d6ef9f4bbe0cc23de0f8c50b7ea79a23e84ed92
MikiKru <michal_kruczkowski@o2.pl>
2020-12-05 18:44:16 +0100

    pai lect 2.1

---
+++ src/test/java/com/example/pai_rest_api/PaiRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pai_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaiRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
