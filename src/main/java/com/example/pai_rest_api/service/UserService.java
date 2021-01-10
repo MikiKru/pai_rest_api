56a7488aef531b22e31f84abdef3ef9bb933753e
MikiKru <michal_kruczkowski@o2.pl>
2021-01-10 19:08:05 +0100

    lecture 10.01.21 v3

--- src/main/java/com/example/pai_rest_api/service/UserService.java
+++ src/main/java/com/example/pai_rest_api/service/UserService.java
@@ -1,14 +1,45 @@
 package com.example.pai_rest_api.service;
 
+import com.example.pai_rest_api.model.User;
+import com.example.pai_rest_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service    // klasa o znaczeniu specjalnym implementująca logikę biznesową
 public class UserService {
-    // SELECT
-
+    UserRepository userRepository;
+    // mechanizm wstrzykiwania zależności - przez konstruktor
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     // INSERT
-
+    public void registerUser(User user){
+        userRepository.save(user);                  // INSERT INTO user VALUES (?,?,?,?,?);
+    }
+    // SELECT
+    public List<User> getAllUsers(){
+        return userRepository.findAll();            // SELECT * FROM user;
+    }
+    public Optional<User> getUserById(int userId){
+        return userRepository.findById(userId);     // SELECT * FROM user WHERE user_id = ?;
+    }
     // UPDATE
-
+    public void activateUser(int userId){
+        Optional<User> userOptional = getUserById(userId);
+        if(userOptional.isPresent()){
+            User userToActivate = userOptional.get();
+            userToActivate.setStatus(true);         // aktywacja
+            userRepository.save(userToActivate);    // UPDATE user SET status = true WHERE user_id = ?;
+        }
+    }
     // DELETE
+    public void deleteUserById(int userId){
+        if(userRepository.existsById(userId)){
+           userRepository.deleteById(userId);       // DELETE FROM user WHERE user_id = ?;
+        }
+    }
 }
